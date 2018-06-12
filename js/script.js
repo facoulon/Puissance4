53f3ec04610c0a3cfa99dfd69bda82fee3d64a5c
PRUNIERES <aurelie.prunieres@gmail.com>
2018-06-12 11:37:37 +0200

    generer tableau de commande

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 // function generate_table() {
 var body = document.getElementsByTagName("body")[0];
 var tbl = document.createElement("table");
+var commandTable = document.createElement("table");
+body.appendChild(commandTable);
 
-
+for (var k = 0; k < 3; k++){
+  var cellC = document.createElement("td");
+  commandTable.appendChild(cellC);
+}
+commandTable.setAttribute('border', '2');
 
 for (var i = 0; i < 3; i++) {
   var row = document.createElement("tr");
   for (var j = 0; j < 3; j++) {
-      var commandTable = document.createElement("div");
-        var cell = document.createElement("td");
-        row.appendChild(cell);
+      var cell = document.createElement("td");
+      row.appendChild(cell);
       }
-      tbl.appendChild(row);
+    tbl.appendChild(row);
     }
-    body.appendChild(commandTable);
     body.appendChild(tbl);
     tbl.setAttribute("border", "2");
-// }
