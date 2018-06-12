1b69f0f58534db9cafcc92244f6e241a9a8f5626
facoulon <f.coulon48@laposte.net>
2018-06-12 14:39:49 +0200

    Some elements add, some remove.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 // function generate_table()
 var body = document.getElementsByTagName("body")[0];
 var tbl = document.createElement("table");
+var nbrColumns = 7;
+var nbrRows = 6;
+
+
 var commandTable = document.createElement("table");
 body.appendChild(commandTable);
 
-for (var k = 0; k < 3; k++){
+var playerSelect = document.createElement("img");
+body.appendChild(playerSelect);
+playerSelect.src = "../img/jaune.png"
+
+for (var k = 0; k < nbrColumns; k++){
   var cellC = document.createElement("td");
   commandTable.appendChild(cellC);
-  cellC.innerHTML = "";
 }
 commandTable.setAttribute('border', '2');
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < nbrRows; i++) {
   var row = document.createElement("tr");
   row.className = "row" + i;
-  for (var j = 0; j < 3; j++) {
+  for (var j = 0; j < nbrColumns; j++) {
       var cell = document.createElement("td");
       cell.className = "cell" + j;
       row.appendChild(cell);
@@ -25,7 +32,9 @@ body.appendChild(tbl);
 tbl.setAttribute("border", "2");
 
 var column = [];
-for (var l = 0; l < 3; l++){
+for (var l = 0; l < nbrColumns; l++){
    column[l] = document.getElementsByClassName('cell'+ l);
-  console.log(column);
 }
+
+
+for (m = 0 ; m < 42)
