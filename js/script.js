af1be6a40d62653db7876173a1ee0c28964bcc3a
facoulon <f.coulon48@laposte.net>
2018-06-12 12:24:19 +0200

    Try, try and try with some reflexion.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// function generate_table() {
+// function generate_table()
 var body = document.getElementsByTagName("body")[0];
 var tbl = document.createElement("table");
 var commandTable = document.createElement("table");
@@ -7,16 +7,34 @@ body.appendChild(commandTable);
 for (var k = 0; k < 3; k++){
   var cellC = document.createElement("td");
   commandTable.appendChild(cellC);
+  cellC.innerHTML = "";
 }
 commandTable.setAttribute('border', '2');
 
 for (var i = 0; i < 3; i++) {
   var row = document.createElement("tr");
+  row.className = "row"+i;
   for (var j = 0; j < 3; j++) {
       var cell = document.createElement("td");
+      cell.className = "cell" +j;
       row.appendChild(cell);
       }
     tbl.appendChild(row);
     }
     body.appendChild(tbl);
     tbl.setAttribute("border", "2");
+
+
+// var plateau = [
+//   ['T','C','F','R','K','F','C','T'],
+//   ['P','P','P','P','P','P','P','P'],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   ['p','p','p','p','p','p','p','p'],
+//   ['t','c','f','k','r','f','c','t'] ];
+//
+// console.log(plateau);
+
+commandTable[0] = "josé";
